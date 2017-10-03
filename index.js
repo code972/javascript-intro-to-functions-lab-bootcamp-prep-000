d9f61e90ea31de19e8f422d86355f4c5988119a9
Paul Cotter <coding97@gmail.com>
2017-10-03 16:11:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function shout(string) {
 return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === .toUpperCase()) {
+  return "I can't hear you!"
+} else
+if (string === .toLowerCase()){
+return "YES INDEED!"
+} else
+if (string === "I love you, Grandma.") {
+  return "I love you, too."
+}
+}
